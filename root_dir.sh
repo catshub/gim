68419d3bb6f5f6ff3bea755fffe07a6b60dda3e7
xiongqi <xiongqi.me@foxmail.com>
2020-01-16 14:47:28 +0800

    feat: root_dir

--- root_dir.sh
+++ root_dir.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 # 获取当前根目录路径
 
-export GIM_HOME=$(
-  cd $(dirname $0)
-  pwd
-)
+if [ -z "$GIM_HOME" -o "$1" == "1" ]; then
+  # no $GIM_HOME or overwrite flag is 1
+  # overwrite $GIM_HOME
+  export GIM_HOME=$(
+    cd $(dirname $0)
+    pwd
+  )
+fi
+echo "GIM_HOME: $GIM_HOME"
