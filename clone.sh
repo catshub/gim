46f043fca39e9297bb2fe5b34edc4f5e0d29d255
xiongqi <xiongqi.me@foxmail.com>
2020-01-16 14:52:17 +0800

    feat: clone

--- clone.sh
+++ clone.sh
@@ -4,13 +4,22 @@
 _gitignore="https://github.com/github/gitignore.git"
 _dir=$GIM_HOME/submodules/gitignore
 
+if [ "$1" != "1" -a -d "$_dir" ]; then
+  # do not overwrite
+  exit 0
+fi
+
 clone_repo() {
   # clone
+  if [ -d "$_dir" ]; then
+    rm -rf $_dir
+  fi
   git clone --single-branch --depth 1 $1 $_dir
 }
 
 clean_repo() {
   if [[ -s $_dir/.git ]]; then
+    echo "clear $_dir/.git"
     rm -rf $_dir/.git
   fi
 }
