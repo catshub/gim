78854000b0993bf1212d0fddb9731702686d8609
xiongqi <xiongqi.me@foxmail.com>
2020-01-16 15:22:54 +0800

    feat: install

--- install.sh
+++ install.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 # 0. 初始化一个 $GIM_HOME 供使用
-bash root_dir.sh
+bash root_dir.sh 1
 
 # 1. clone gitignore repo
-bash clone.sh
+bash clone.sh 1
 
-# # 2. create indexes
+# 2. create indexes
 bash create_indexes.sh
+
+# copy to ~/.zshrc
+if [ -z "$(cat ~/.zshrc | grep GIM_HOME)" ]; then
+  echo "" >>~/.zshrc
+  echo "export GIM_HOME=$GIM_HOME" >>~/.zshrc
+  echo "PATH=\$PATH:\$GIM_HOME/bin" >>~/.zshrc
+  echo "[ -s \$GIM_HOME/gim_completion.sh ] && . \$GIM_HOME/gim_completion.sh" >>~/.zshrc
+  echo "" >>~/.zshrc
+fi
