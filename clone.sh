96dd7bc3ebb2bdd04a0661d2f6e08d5f7203b0d3
xiongqi <xiongqi.me@foxmail.com>
2020-01-16 09:47:00 +0800

    feat: clone repo

--- clone.sh
+++ clone.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 # clone子仓库
 
-gitignore=https://github.com/github/gitignore.git
+_gitignore="https://github.com/github/gitignore.git"
+_dir=$GIM_HOME/submodules/gitignore
 
-git clone $gitignore --single-branch --depth 1
+clone_repo() {
+  # clone
+  git clone --single-branch --depth 1 $1 $_dir
+}
+
+clean_repo() {
+  if [[ -s $_dir/.git ]]; then
+    rm -rf $_dir/.git
+  fi
+}
+
+_do() {
+  # 1. clone repo
+  clone_repo $_gitignore
+  # 2 clean repo
+  clean_repo
+}
+
+_do
