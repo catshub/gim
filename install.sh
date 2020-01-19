3aa318306749725e038c7da8a682ea9b316fbcb2
xiongqi <xiongqi.me@foxmail.com>
2020-01-19 10:25:27 +0800

    fix: brew install error

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# 0. 初始化一个 $GIM_HOME 供使用
-bash root_dir.sh 1
+# 0. 创建根目录
+bash root_dir.sh
 
 # 1. clone gitignore repo
 bash clone.sh 1
@@ -14,7 +14,7 @@ if [ -z "$(cat ~/.zshrc | grep GIM_HOME)" ]; then
   echo "" >>~/.zshrc
   user_dir=$(echo ~)
   echo "export GIM_HOME=${GIM_HOME/${user_dir}/~}" >>~/.zshrc
-  echo "PATH=\$PATH:\$GIM_HOME/bin" >>~/.zshrc
+  # echo "PATH=\$PATH:\$GIM_HOME/bin" >>~/.zshrc
   echo "[ -s \$GIM_HOME/gim_completion.sh ] && . \$GIM_HOME/gim_completion.sh" >>~/.zshrc
   echo "" >>~/.zshrc
 fi
--- root_dir.sh
+++ root_dir.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
-# 获取当前根目录路径
+# 获取当前根目录路径创建根目录
 
-if [ -z "$GIM_HOME" -o "$1" = "1" ]; then
-  # no $GIM_HOME or overwrite flag is 1
-  # overwrite $GIM_HOME
-  export GIM_HOME=$(
-    cd $(dirname $0)
-    pwd
-  )
+export GIM_HOME=~/.gim
+if [ ! -d "$GIM_HOME" ]; then
+  mkdir $GIM_HOME
 fi
 echo "GIM_HOME: $GIM_HOME"
