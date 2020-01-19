ec3511d0d57c9aae04afc6b4228e14e119379eec
xiongqi <xiongqi.me@foxmail.com>
2020-01-19 14:26:39 +0800

    fix: local_install

--- local_install.sh
+++ local_install.sh
@@ -5,14 +5,31 @@ export GIM_HOME=~/.gim
 if [ ! -d "$GIM_HOME" ]; then
   mkdir $GIM_HOME
 fi
+cd $GIM_HOME
 echo "GIM_HOME: $GIM_HOME"
 
-# 1. clone gitignore repo
-bash clone.sh 1
+# 1. get gitignore repo
+## bash clone.sh 1
+gitignore_url="https://github.com/github/gitignore/archive/master.zip"
+gitignore_zip=gitignore.zip
+sub_dir=submodules/gitignore
+tmp_dir=$(mktemp -d submodules.XXXXXXXXXX)
+curl -L -o $GIM_HOME/$gitignore_zip $gitignore_url
+tar -xf $GIM_HOME/$gitignore_zip -C $GIM_HOME/$tmp_dir
+[ ! -d $sub_dir ] && mkdir $sub_dir
+mv $tmp_dir/*/* $sub_dir
+## clean
+rm $tmp_dir
+rm $gitignore_zip
 
 # 2. create indexes
-bash create_indexes.sh
+## 读取所有模板文件并转化为小写
+_FILES=($(ls $GIM_HOME/submodules/gitignore | grep '.gitignore$' | tr A-Z a-z))
+## 去除尾部
+_FILES_NAME=${_FILES[@]/.gitignore/} # _FILES_NAME=${_FILES[@]%.gitignore}
+echo $_FILES_NAME > $GIM_HOME/indexes
 
+# 3. 更新 .zshrc
 # copy to ~/.zshrc
 if [ -z "$(cat ~/.zshrc | grep GIM_HOME)" ]; then
   echo "" >>~/.zshrc
@@ -23,5 +40,5 @@ if [ -z "$(cat ~/.zshrc | grep GIM_HOME)" ]; then
   echo "" >>~/.zshrc
 fi
 
-#
-cp $GIM_HOME/bin/gim /usr/local/bin/gim
+# end
+cp $GIM_HOME/bin/gim /usr/local/bin/
--- root_dir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# 获取当前根目录路径创建根目录
-
-export GIM_HOME=~/.gim
-if [ ! -d "$GIM_HOME" ]; then
-  mkdir $GIM_HOME
-fi
-echo "GIM_HOME: $GIM_HOME"
